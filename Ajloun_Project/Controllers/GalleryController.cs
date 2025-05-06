6d544cfd48d2508a464c253974a0d951f508c442
Toqa Khashashneh <toqamusa@gmail.com>
2025-05-06 18:56:35 +0300

    gallery

---
+++ Ajloun_Project/Controllers/GalleryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ajloun_Project.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
