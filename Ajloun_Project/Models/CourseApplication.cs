0fb2e930a4603093483f0d9d3c1ed58588bffa57
Mona-Tamimi <munatamimi65@gmail.com>
2025-05-11 12:04:34 +0300

    home and articles

---
+++ Ajloun_Project/Models/CourseApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ajloun_Project.Models;
+
+public partial class CourseApplication
+{
+    public int ApplicationId { get; set; }
+
+    public int CourseId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string? BirthCertificateImage { get; set; }
+
+    public bool Agreement { get; set; }
+
+    public string? Status { get; set; }
+
+    public DateTime? SubmittedAt { get; set; }
+
+    public virtual Course Course { get; set; } = null!;
+
+    public virtual User? User { get; set; }
+}
---
+++ Ajloun_Project/Models/Handicraft.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ajloun_Project.Models;
+
+public partial class Handicraft
+{
+    public int CraftId { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public int? Stock { get; set; }
+
+    public string? Category { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<CraftOrder> CraftOrders { get; set; } = new List<CraftOrder>();
+}
