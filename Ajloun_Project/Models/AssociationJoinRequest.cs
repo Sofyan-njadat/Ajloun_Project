c6f6e7542a22c2ad7a4f84c4a8be5359bb2523cb
Sofyan Njadat <sofyan.njfb@gmail.com>
2025-05-12 21:11:16 +0300

    Sfn

---
+++ Ajloun_Project/Models/AssociationJoinRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ajloun_Project.Models;
+
+public partial class AssociationJoinRequest
+{
+    public int RequestId { get; set; }
+
+    public int? AssociationId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string? Status { get; set; }
+
+    public virtual CulturalAssociation? Association { get; set; }
+
+    public virtual User? User { get; set; }
+}
